77c7ed85068642b2ad175125549c9f3e01a11b42
Jim Anderson <rumsontrader@gmail.com>
2017-08-06 15:36:59 -0400

    Finished TriangleArea Project

--- TriangleArea/Program.cs
+++ TriangleArea/Program.cs
@@ -10,7 +10,18 @@ namespace TriangleArea
     {
         static void Main(string[] args)
         {
+            double areaOfTriangle, baseOfTriangle, heightOfTraingle ;
+            const float oneHalfF = .5F;
 
+            baseOfTriangle = 20.3658;
+            heightOfTraingle = 6.52222;
+
+            areaOfTriangle = oneHalfF * (baseOfTriangle * heightOfTraingle);
+
+            Console.WriteLine("The base of the triangle is " + baseOfTriangle + " inches");
+            Console.WriteLine("The height of the triangle is " + heightOfTraingle + " inches");
+            Console.WriteLine("The area of the triangle is " + areaOfTriangle + " Square Inches");
+            Console.ReadLine();
 
         }
     }
