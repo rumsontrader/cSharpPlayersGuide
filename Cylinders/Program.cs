e69f21260923e12c354a7d30f14dde2e6c8894e8
Jim Anderson <rumsontrader@gmail.com>
2017-08-09 17:59:50 -0400

    Added Cylinders Project

---
+++ Cylinders/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cylinders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double volume, surfaceArea;
+            double pi = 3.14;
+            string userInput;
+
+        }
+    }
+}
