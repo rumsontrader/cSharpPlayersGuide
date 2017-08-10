15886147f5224e380a5213f0f62d670fd6d26b83
Jim Anderson <rumsontrader@gmail.com>
2017-08-09 20:45:53 -0400

    Completed cylinder project

--- Cylinders/Program.cs
+++ Cylinders/Program.cs
@@ -10,9 +10,38 @@ namespace Cylinders
     {
         static void Main(string[] args)
         {
-            double volume, surfaceArea;
-            double pi = 3.14;
-            string userInput;
+            double volume, surfaceArea, height, radius;
+            double pi = 3.1415926535897931;
+            string userHeightInput, userRadiusInput, 
+                askForRadius = "What is the radius of your cylinder in inches?", 
+                askForHeight = "What is the height of yur cylinder in inches?";
+
+            Console.WriteLine("This program will determine the volume " +
+                "and the surface area of your cylinder ");
+            Console.WriteLine("");
+
+            Console.WriteLine(askForHeight);
+            userHeightInput = Console.ReadLine();
+            height = Convert.ToDouble(userHeightInput);
+
+            Console.WriteLine(askForRadius);
+            userRadiusInput = Console.ReadLine();
+            radius = Convert.ToDouble(userRadiusInput);
+
+            volume = pi * (radius * radius) * height;
+            surfaceArea = 2 * pi * radius * (radius + height);
+
+            Console.WriteLine("The volume of your cylinder is " + volume + " cubic inches");
+            Console.WriteLine();
+
+            Console.Write("The surface area or your cylinder is " 
+                + surfaceArea + " cubic inches");
+
+
+            Console.ReadLine();
+
+
+
 
         }
     }
