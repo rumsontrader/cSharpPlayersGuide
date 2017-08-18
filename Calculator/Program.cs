8e7aa206f25f26ef6875eb019b7816d641a3e507
Jim Anderson <rumsontrader@gmail.com>
2017-08-18 14:48:49 -0400

    Completed calculator project

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,12 +12,14 @@ namespace Calculator
         static void Main(string[] args)
         {
 
-        string firstNumber, secondNumber;
+        string firstNumber, secondNumber, operater;
         double operandOne, operandTwo;
-        double addition, subtraction, multiplication, division, modulo, exponent;
         char add = '+', sub = '-', mul = '*', div = '/', mod = '%', exp = '^';
+        char input;
 
             Console.WriteLine("This program will do Calculations for you ");
+            Console.WriteLine("If you want and exponent the second number should be the power");
+            Console.WriteLine("you want the first number raised to  ");
             Console.WriteLine("Type in your first number: ");
             firstNumber = Console.ReadLine();
             operandOne = Convert.ToDouble(firstNumber);
@@ -31,11 +33,43 @@ namespace Calculator
                 " for multiplication, " + div + " for division, " + mod + 
                 " for remainder, " + exp + " for exponent. ");
 
+            operater = Console.ReadLine();
+            input = Convert.ToChar(operater);
 
+            switch (input)
+            {
+                case '+':
+                    Console.WriteLine(operandOne + " Plus " + operandTwo + " = " + (operandOne + operandTwo));
+                    break;
 
+                case '-':
+                    Console.WriteLine(operandOne + " Minus " + operandTwo + " = " + (operandOne - operandTwo));
+                    break;
 
-            //Console.WriteLine("This program will do Calculations for you ");
+                case '*':
+                    Console.WriteLine(operandOne + " Times " + operandTwo + " = " + (operandOne * operandTwo));
+                    break;
 
+                case '/':
+                    if(operandTwo == 0)
+                    {
+                        Console.Write("You can not divide by zero ");
+                        break;
+                    }
+                    Console.WriteLine(operandOne + " Divided by " + operandTwo + " = " + (operandOne / operandTwo));
+                    break;
+
+                case '%':
+                    Console.WriteLine("Remainder of " + operandOne + " Divided by " + operandTwo + " = " + (operandOne % operandTwo));
+                    break;
+
+                case '^':
+                    Console.WriteLine(operandOne + " To the Power of " + operandTwo + " = " + (operandOne = Math.Pow(operandOne, operandTwo)));
+                    break;
+                    default:                
+                    Console.WriteLine(" Something went wrong ");
+                    break;               
+            }
             Console.ReadLine();
         }
     }
