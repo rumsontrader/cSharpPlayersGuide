a69b0ee56f5ebe51d918c24cfc63a62a68e7ede9
Jim Anderson <rumsontrader@gmail.com>
2017-08-20 12:32:07 -0400

    Finished Project

---
+++ ArrayCopy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayCopy
+{
+    class Program
+    {
+        private static int index;
+        private static int firstIndex;
+
+        static void Main(string[] args)
+        {
+            int[] firstArray = new int[10] { 3, 6, 9, 12, 15, 18, 21, 24, 27, 92 };           
+            int[] secondArray = new int[10];
+
+            Console.Write(firstArray[firstIndex] + " ");
+            Console.WriteLine();
+
+            for (int firstIndex = 0; firstIndex < firstArray.Length; firstIndex++)
+            {
+                Console.Write(firstArray[firstIndex] + " ");            
+            }
+            Console.WriteLine();
+
+                for (int firstIndex = 0; firstIndex < firstArray.Length; firstIndex++)
+            {
+                secondArray[index] = firstArray[firstIndex];
+                Console.Write(secondArray[index] + " ");
+            }            
+            Console.ReadLine();
+        }
+    }
+}
